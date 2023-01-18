0d0d64798118edfcc55f1a46ea5a3b992f4a24bb
swaplasurkar <lasurkars@gmail.com>
2023-01-18 10:29:55 +0530

    modified

--- calculator.py
+++ calculator.py
@@ -1,5 +1,17 @@
 def add(num1,num2):
     return num1+num2
 
+def sub(num1,num2):
+    return num1-num2
+
+def mul(num1,num2):
+    return num1*num2
+
 res_add=add(10,20)
 print(res_add)
+
+res_sub=sub(40,10)
+print(res_sub)
+
+res_mul=mul(3,10)
+print(res_mul)
